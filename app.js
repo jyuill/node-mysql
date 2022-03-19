4a12aec732edb2add002c0fe63aa8a18ce03d7dd
John Yuill <john@fig4.com>
2022-03-18 23:42:01 -0700

    first function

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ var msg2 = "Loop it!"
 for(i=0; i < 5; i++){
     console.log(msg2);
 }
+
+// load faker package (already installed)
+const {faker}=require('@faker-js/faker')
+// demo faker
+console.log(faker.internet.email());
+console.log(faker.date.past());
+
+function generateAddress(){
+    console.log(faker.address.streetAddress());
+    console.log(faker.address.city());
+    console.log(faker.address.country());
+}
+
+generateAddress();
